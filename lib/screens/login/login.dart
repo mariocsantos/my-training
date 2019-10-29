1754e699f92def66519fbc10cdc49586e429cce4
Mario Santos <mariocsants@gmail.com>
2019-10-29 16:16:33 -0300

    Layout fix in login screen

--- lib/screens/login/login.dart
+++ lib/screens/login/login.dart
@@ -21,80 +21,97 @@ class LoginPageState extends State<LoginPage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              Container(
-                padding: EdgeInsets.only(left: paddingSide, right: paddingSide),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    // Logo
-                    Container(
-                      padding: EdgeInsets.only(top: 48, bottom: 48),
-                      child: Text(
-                        'MY TRAINING',
-                        style: TextStyle(
-                            fontWeight: FontWeight.w300, fontSize: 24),
-                      ),
-                    ),
-                    // Login info
-                    Container(
-                      margin: EdgeInsets.only(bottom: 24),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            'Login',
-                            style: TextStyle(fontSize: 20),
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.only(
+                    left: paddingSide,
+                    right: paddingSide,
+                    top: 36,
+                    bottom: 36,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      // Logo
+                      Container(
+                        child: Text(
+                          'MY TRAINING',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w300,
+                            fontSize: 24,
                           ),
-                          Container(
-                            margin: EdgeInsets.only(top: 4),
-                            child: Text(
-                              'Escreva seu e-mail e senha para recuperar seus treinos.',
-                              style: TextStyle(
-                                fontSize: 12,
-                                fontWeight: FontWeight.w300,
+                        ),
+                      ),
+                      Container(
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                              margin: EdgeInsets.only(
+                                bottom: 24,
+                                top: 24,
+                              ),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Text(
+                                    'Login',
+                                    style: TextStyle(fontSize: 20),
+                                  ),
+                                  Container(
+                                    margin: EdgeInsets.only(top: 4),
+                                    child: Text(
+                                      'Escreva seu e-mail e senha para recuperar seus treinos.',
+                                      style: TextStyle(
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.w300,
+                                      ),
+                                    ),
+                                  ),
+                                ],
                               ),
                             ),
-                          ),
-                        ],
+                            LoginForm(),
+                          ],
+                        ),
                       ),
-                    ),
-                    LoginForm(),
-                    Container(
-                      width: double.infinity,
-                      padding: EdgeInsets.only(top: 42, bottom: 42),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          Text(
-                            'Ou treine usando uma rede social.',
-                            style: TextStyle(
-                                fontSize: 12, fontWeight: FontWeight.w300),
-                          ),
-                          Container(
-                            margin: EdgeInsets.only(top: 24),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Container(
-                                  margin: EdgeInsets.only(right: 24),
-                                  child: FloatingActionButton(
-                                    backgroundColor: Colors.red,
-                                    onPressed: () {},
-                                    child: Icon(CustomIcons.google),
-                                  ),
-                                ),
-                                FloatingActionButton(
-                                  backgroundColor: Colors.blue[700],
-                                  onPressed: () {},
-                                  child: Icon(CustomIcons.facebook),
-                                ),
-                              ],
+                      Container(
+                        width: double.infinity,
+                        padding: EdgeInsets.only(top: 0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              'Ou treine usando uma rede social.',
+                              style: TextStyle(
+                                  fontSize: 12, fontWeight: FontWeight.w300),
                             ),
-                          )
-                        ],
+                            Container(
+                              margin: EdgeInsets.only(top: 16),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Container(
+                                    margin: EdgeInsets.only(right: 24),
+                                    child: FloatingActionButton(
+                                      backgroundColor: Colors.red,
+                                      onPressed: () {},
+                                      child: Icon(CustomIcons.google),
+                                    ),
+                                  ),
+                                  FloatingActionButton(
+                                    backgroundColor: Colors.blue[700],
+                                    onPressed: () {},
+                                    child: Icon(CustomIcons.facebook),
+                                  ),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
               Container(
